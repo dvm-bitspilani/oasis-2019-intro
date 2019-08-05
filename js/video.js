e438272d5f54f77dd7387dff3658500667864ec7
Akshit-Patel <akshitpatel72@gmail.com>
2019-08-05 19:55:03 +0530

    carousel without animations

--- js/video.js
+++ js/video.js
@@ -1,26 +1,72 @@
-'use strict';
+// 'use strict';
 var screenWidth = window.innerWidth;
-let cards = document.getElementsByClassName("cards");
-console.log(screenWidth);
+var cards = document.getElementsByClassName("cards");
+var currentCard = 1;
+var numberOfCards = cards.length;
 
 window.addEventListener("resize",()=> {
-    screenWidth = window.innerWidth;
-   
+    // screenWidth = window.innerWidth;
+    currentCard = 1;
+    if(screenWidth < 700){
+        // console.log(cards);
+        for(let i=1 ; i < cards.length ; i++){
+            console.log(i);
+            cards[i].style.display = "none";
+        }
+        cards[0].style.display = "grid";
+    }else{
+        console.log("dssds");
+        for(var i=0; i< cards.length ; i++){
+            cards[i].style.display = "grid";
+        }
+    }   
 });
 
 if(screenWidth < 700){
-    console.log("dssds");
-    let cards = document.getElementsByClassName("cards");
-    console.log(cards);
-    for(let i=1 ; i < cards.length ; ++i){
+    // console.log(cards);
+    currentCard = 1;    
+    for(let i=1 ; i < cards.length ; i++){
         console.log(i);
-        cards[i].style.display="none";
+        cards[i].style.display = "none";
     }
-
+    cards[currentCard-1].style.display = "grid";
 }else{
     console.log("dssds");
     for(var i=0; i< cards.length ; i++){
         cards[i].style.display = "grid";
-        console.log(i);
     }
-}   
+} 
+
+var leftButton = document.getElementsByClassName("arrow-left")[0];
+var rightButton = document.getElementsByClassName("arrow-right")[0];
+rightButton.addEventListener("click",next);
+leftButton.addEventListener("click",back);
+
+
+function next(){
+    currentCard = currentCard+1;
+    if(currentCard > numberOfCards){
+        currentCard = 1;
+    }
+    for(let i=0 ; i < cards.length ; i++){
+        console.log(i);
+        cards[i].style.display = "none";
+    }
+    cards[currentCard-1].style.display = "grid";
+}
+
+function back(){
+    currentCard = currentCard-1;
+    if(currentCard == 0){
+        currentCard = cards.length;
+    }
+    for(let i=0 ; i < cards.length ; i++){
+        console.log(i);
+        cards[i].style.display = "none";
+    }
+    cards[currentCard-1].style.display = "grid";
+}
+
+
+
+
