53a2623655e4963b11d20768230a6b88b7112338
meliodas113 <f20180433@pilani.bits-pilani.ac.in>
2019-08-07 20:11:49 +0530

    added scroller

--- js/index.js
+++ js/index.js
@@ -1,52 +1,55 @@
-window.onload = function() {  
-    let loader = document.getElementsByClassName("loader")[0];
-    let about = document.getElementsByClassName("about")[0];
-    let landingWrapper = document.getElementsByClassName("landing-wrapper")[0];
-    landingWrapper.style.transition = "opacity ease-out 0.5s"; // fade-in effect for content on loading
-    about.style.transition = "opacity ease-out 0.5s"; // fade-in effect for content on loading
-  
-    setTimeout(() => {
-      document.getElementsByTagName('body')[0].style.overflowY = 'auto';
-      loader.style.display="none";
-      landingWrapper.style.opacity = 1;
-      about.style.opacity = 1;
-      // document.getElementsByClassName("nav-bar")[0].style.display = "flex";
-      // document.getElementsByClassName("sidebar")[0].style.display = "flex";
-      // if (window.innerWidth > 500)
-      //   document.getElementsByClassName("navigator")[0].style.display = "flex";
-      // if (window.innerWidth < 500) {
-      //   document.getElementsByClassName("navigator")[0].style.display = "none";
-      // }
-    }, 4500);
+window.onload = function() {
+  let loader = document.getElementsByClassName("loader")[0];
+  let about = document.getElementsByClassName("about")[0];
+  let landingWrapper = document.getElementsByClassName("landing-wrapper")[0];
+  landingWrapper.style.transition = "opacity ease-out 0.5s"; // fade-in effect for content on loading
+  about.style.transition = "opacity ease-out 0.5s"; // fade-in effect for content on loading
+
+  setTimeout(() => {
+    document.getElementsByTagName("body")[0].style.overflowY = "auto";
+    loader.style.display = "none";
+    document.getElementsByClassName("bottom-div")[0].style.opacity = "1";
+    landingWrapper.style.opacity = 1;
+    about.style.opacity = 1;
+    // document.getElementsByClassName("nav-bar")[0].style.display = "flex";
+    // document.getElementsByClassName("sidebar")[0].style.display = "flex";
+    // if (window.innerWidth > 500)
+    //   document.getElementsByClassName("navigator")[0].style.display = "flex";
+    // if (window.innerWidth < 500) {
+    //   document.getElementsByClassName("navigator")[0].style.display = "none";
+    // }
+  }, 4500);
 };
 
 let hue = 0;
 setInterval(() => {
-  if(hue < 360) 
-    hue += 30;
-  else
-    hue = 0;
+  if (hue < 360) hue += 30;
+  else hue = 0;
 
-  document.getElementsByClassName('landing-wrapper')[0].style.transition = '1s';
-  document.getElementsByClassName('landing-wrapper')[0].style.filter = 'hue-rotate(' + hue + 'deg)';
+  document.getElementsByClassName("landing-wrapper")[0].style.transition = "1s";
+  document.getElementsByClassName("landing-wrapper")[0].style.filter =
+    "hue-rotate(" + hue + "deg)";
 }, 5000);
-
 
 var nav = document.getElementsByClassName("navigation")[0];
 var isNavOpen = false;
 function toogleNav() {
-  if(!isNavOpen) {
+  if (!isNavOpen) {
     isNavOpen = true;
     openNav();
-    document.querySelectorAll("#hamburger > div")[0].style.transform="rotate(45deg)";
-    document.querySelectorAll("#hamburger > div")[2].style.transform="rotate(-45deg)";
-    document.querySelectorAll("#hamburger > div")[1].style.opacity="0";
+    document.querySelectorAll("#hamburger > div")[0].style.transform =
+      "rotate(45deg)";
+    document.querySelectorAll("#hamburger > div")[2].style.transform =
+      "rotate(-45deg)";
+    document.querySelectorAll("#hamburger > div")[1].style.opacity = "0";
   } else {
     isNavOpen = false;
     closeNav();
-    document.querySelectorAll("#hamburger > div")[0].style.transform="rotate(0deg)";
-    document.querySelectorAll("#hamburger > div")[2].style.transform="rotate(0deg)";
-    document.querySelectorAll("#hamburger > div")[1].style.opacity="1";
+    document.querySelectorAll("#hamburger > div")[0].style.transform =
+      "rotate(0deg)";
+    document.querySelectorAll("#hamburger > div")[2].style.transform =
+      "rotate(0deg)";
+    document.querySelectorAll("#hamburger > div")[1].style.opacity = "1";
   }
 }
 
@@ -54,5 +57,5 @@ function openNav() {
   nav.style.transform = "translateX(0)";
 }
 function closeNav() {
-  nav.style.transform = "translateX(-100vw)"
+  nav.style.transform = "translateX(-100vw)";
 }
