f61c82ff9769f9c4c7206735ce9c2e13bc122e27
meliodas113 <f20180433@pilani.bits-pilani.ac.in>
2019-08-09 07:36:08 +0530

    events list added

--- js/register.js
+++ js/register.js
@@ -1,5 +1,6 @@
 window.onload = function() {
   const college_select = document.getElementById("city_opt");
+  const events_select = document.getElementById("events_opt");
   const URL = "https://bits-oasis.org/registrations/get_college/";
   const URL2 = "https://bits-oasis.org/registrations/events_details/";
 
@@ -21,8 +22,22 @@ window.onload = function() {
     .then(resp => resp.json())
     .then(function(response) {
       console.log(response);
+      for (var i = 0; i < response.length; i++) {
+        for (var j = 0; j < response[i].events.length; j++) {
+          console.log(response[i].events[j].name);
+          var opt = document.createElement("option");
+          opt.value = response[i].events[j].id;
+          opt.innerHTML = response[i].events[j].name;
+          events_select.add(opt);
+        }
+      }
     })
     .catch(function(error) {
       console.log(error);
     });
 };
+
+function getcollegeid() {
+  const val = document.getElementById("city_opt").value;
+  console.log(val);
+}
